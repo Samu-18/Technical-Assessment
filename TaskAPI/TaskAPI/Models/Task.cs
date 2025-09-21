cd5cb8bcf352787e8a7e91c6fc825eaf59cd8177
Samukelile <130485647+Samu-18@users.noreply.github.com>
2025-09-21 14:11:27 +0200

    Part 2

--- TaskAPI/TaskAPI/Models/Task.cs
+++ TaskAPI/TaskAPI/Models/Task.cs
@@ -2,8 +2,8 @@
 {
     public class Task
     {
-        public int Id { get; set; }                     
-        public string Title { get; set; } 
+        public int Id { get; set; }
+        public string Title { get; set; } = ""; 
         public string? Description { get; set; }
         public int? AssigneeUserId { get; set; }        // FK 
         public DateTime? DueDate { get; set; }
