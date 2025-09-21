9619bb58ff08610a6f1636d48c4827e3dacc9a30
Samukelile <130485647+Samu-18@users.noreply.github.com>
2025-09-21 12:58:59 +0200

    Part1

---
+++ TaskAPI/TaskAPI/Controllers/AppContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace TaskAPI.Controllers
+{
+    public class AppContext
+    {
+        public AppContextDb(DbContextOptions<AppContextDb> options) : base(options) { }
+
+        public DbSet<User> Users => Set<User>();
+        public DbSet<Task> Tasks => Set<Task>();
+    }
+}
